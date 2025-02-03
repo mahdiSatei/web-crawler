9ba5ddcde6af2db3ec94c383bfa259e24e006c56
mahdi satei <mahdisatei82@gmail.com>
2025-02-03 13:34:46 +0330

    add a function to get links of a specific page

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from time import sleep
 
+# Setup chrom web driver
 PATH = "./chromedriver"
 chrom_options = Options()
 service = Service(PATH)
@@ -11,3 +12,18 @@ chrom_options.add_argument("--headless")
 driver = webdriver.Chrome(service=service, options=chrom_options)
 
 base_url = "https://www.isna.ir"
+
+collected_links = set()
+
+# A function to get links of page
+def get_links(url):
+    links = set()
+    driver.get(url)
+
+    elements = driver.find_elements(By.TAG_NAME, "a")
+    for elem in elements:
+        link = elem.get_attribute("href")
+        if link:
+            links.add(link)
+    
+    return links
